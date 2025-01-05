931dcad32a655d61970586d21400d6d4ecfc6e58
mukesh441 <sethi.mukeshjain@gmail.com>
2025-01-05 12:49:16 +0530

    second commit

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -7,14 +7,14 @@ def test_add():
     assert add(3, 5) == 8
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
-    print('add test successful')
+
 
 def test_subtract():
     """Test the subtract function."""
     assert subtract(10, 4) == 6
     assert subtract(0, 5) == -5
     assert subtract(5, 5) == 0
-    print('Sub test successful')
+
 
 if __name__ == "__main__":
     pytest.main()
